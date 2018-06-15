55f9614a8b30ed0e05349119e6b5a9f560349109
Ritika Dhyani <39532567+RitikaDhyani@users.noreply.github.com>
2018-06-16 00:18:38 +0530

    Initial commit

---
+++ program1.py
@@ -0,0 +1,16 @@
+print('s = "abcdef"')
+
+s = "abcdef"
+
+print("s[0] =",s[0])
+print("s[0:3] =",s[0:3])
+print("s[0:4:2] =",s[0:4:2])
+
+print("s.find('c') = ",s.find('c'))
+print("s.replace('d','z') =",s.replace('d','z'))
+print("s.count('e') =",s.count('e'))
+print("s.capitalize() =",s.capitalize())
+s = "ABCDEF"
+print('s = "ABCDEF"')
+print("s.casefold() =",s.casefold())
+print("s =",s)
